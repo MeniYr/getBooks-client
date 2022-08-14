076e7fc5e3bd0cba83c4d3142a28a92e0b833b71
meni rotblat <meny1717@gamil.com>
2022-08-14 22:25:16 +0530

    s

--- get_books/src/componets/model.jsx
+++ get_books/src/componets/model.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { closeModel } from '../shared/redux/features/modelSlice'
+
+
 
 export default function Model() {
+    const dispatch = useDispatch()
+
     return (
         <aside className='model-container bg-gradient'>
             <div className="model">
                 <div className="btn container">
-                    <h4>hre you sure?</h4>
-                    <button className="btn confirm-btn">
+                    <h4>Are you sure?</h4>
+                    <button 
+                    onClick={()=>{dispatch(closeModel())}}
+                    className="btn confirm-btn">
                         אישור
                     </button>
-                    <button className='btn clear-btn'>
+                    <button 
+                     onClick={()=>{dispatch(closeModel())}}
+                    className='btn clear-btn'>
                         בטל
                     </button>
                 </div>
