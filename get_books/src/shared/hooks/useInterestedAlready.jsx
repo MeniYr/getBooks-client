475f106ee08b276b7fa38268921b7e5f4629a2b5
meni rotblat <meny1717@gamil.com>
2022-08-15 18:44:19 +0300

    a

--- get_books/src/shared/hooks/useInterestedAlready.jsx
+++ get_books/src/shared/hooks/useInterestedAlready.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+//TODO check if there same properties of the interested user (same book and same user on same owner)
 
 
 export default function useInterestedAlready() {
